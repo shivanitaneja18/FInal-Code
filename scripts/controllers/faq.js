b17a12a1549a0551fd5de9400eae7921d427a63f
Shivani Taneja <shivani.taneja104@gmail.com>
2017-12-30 21:42:15 +0530

    Create faq.js

---
+++ scripts/controllers/faq.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name halanxApp.controller:FaqCtrl
+ * @description
+ * # FaqCtrl
+ * Controller of the halanxApp
+ */
+angular.module('halanxApp')
+  .controller('FaqCtrl', function ($scope,faq) {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    onLoad();
+    $scope.showMe = false;
+    $scope.hideMe = true;
+    function onLoad(){
+     var promise =  faq.getCategory();
+     promise.then((data)=>{
+       $scope.categories = data;
+       console.log(data);
+     })
+    };
+
+    $scope.showTopic = (slug)=>{
+      $scope.slug = slug;
+      var promise = faq.getTopic(slug);
+      promise.then((data)=>{
+        $scope.hideMe = true;
+        $scope.showMe = false;
+        $scope.topic = data;
+        console.log(data);
+      })
+    }
+
+    $scope.showAnswer = (slug1)=>{
+      var promise = faq.getQuestion($scope.slug,slug1);
+      promise.then((data)=>{
+        $scope.showMe = true;
+        $scope.hideMe = false;
+        console.log(data);
+        $scope.answer = data;
+      })
+    }
+
+  });
