9d3886c189f2020413cf9ea7ab6c01b3d310ba6c
Shivani Taneja <shivani.taneja104@gmail.com>
2018-01-20 13:00:28 +0530

    Update orders.js

--- scripts/controllers/orders.js
+++ scripts/controllers/orders.js
@@ -9,6 +9,7 @@
  */
 angular.module('halanxApp')
   .controller('OrdersCtrl', function ($scope,orders,$window) {
+      $scope.loader = true;
        if(localStorage.getItem("isLogin") === null || JSON.parse(localStorage.getItem("isLogin"))==false){
      $window.location.href = "#login";
     }
@@ -19,10 +20,15 @@ angular.module('halanxApp')
         else{
             $scope.counter = 0;
         }
+    $scope.past = true;
+    $scope.check1;
+    $scope.showCancel = false;
+    // $scope.cancel_order = true;    
     $scope.rev = true;
     $scope.menu = false;
     $scope.movex = true;
-
+    $scope.cancelMsg = "Are you sure, you want to cancel the order?";
+    $scope.order_id;
     $scope.addsidebar = ()=>{
         $scope.movex = !$scope.movex;
     }
@@ -37,6 +43,7 @@ angular.module('halanxApp')
     $scope.toggleRec = true;
 
     $scope.trackyourOrder = (order)=>{
+        $scope.loader = false;
         $scope.trackToggle = true;
         $scope.toggleDeliver = true;
         $scope.toggleOnGo = true;
@@ -49,31 +56,41 @@ angular.module('halanxApp')
                      
             promise.then(function(data){
                 console.log("mere func k andr");
-        console.log(data[0]);
+        console.log(data);
 
         if(data=="") {
             $scope.toggleRec = false;
             $scope.trackStatus = "Order Recieved";
+            $scope.loader = true;
         } else {
             if(data[0].IsDelivered==false) {
                 console.log("delivered");
                 $scope.trackStatus = "On Going";
+                if(data[0].ShopperId){
                 $scope.delby = ((data[0].ShopperId).user).first_name +" "+ ((data[0].ShopperId).user).last_name;
-                $scope.orderItems = data[0].items;
                 $scope.shopperMo = (data[0].ShopperId).PhoneNo;
+                $scope.estimate = data[0].ETA;
+                }
+                $scope.orderItems = data[0].items;
+                
                 $scope.toggleOnGo = false;
+                $scope.loader = true;
             }else if(data[0].IsDelivered==true) {
                 console.log("ongoing");
                 $scope.trackStatus = "Order Delivered";
                 $scope.toggleDeliver = false;
+                $scope.loader = true;
             }
         }
       },function(err){
   
     } ); 
 
-    }  
-
+}  
+$scope.$on('$viewContentLoaded', function(){
+    $scope.myorder("ongoing");
+  });
+    
     $scope.myorder = (check)=>{
         if(localStorage.token){
             let token = orders.gettoken();
@@ -81,6 +98,7 @@ angular.module('halanxApp')
         var promise = orders.getorderon(token);
         
         if(check=="ongoing"){
+            $scope.past = false;
             $scope.noongo=false;
              var filterdata = [];
              var k=0;
@@ -89,7 +107,7 @@ angular.module('halanxApp')
                 for(var i=0;i<data.length;i++){
                      for(var j=0;j<data[i].order_items.length;j++){
                         //  data[i].Total = data[i].Total.toFixed(2);
-                          if(data[i].order_items[j].IsDelivered==false){
+                          if(data[i].order_items[j].IsDelivered==false && data[i].IsCancelled == false){
                               
                             //   console.log(data[i]);
                               filterdata[k] = data[i];
@@ -112,6 +130,7 @@ angular.module('halanxApp')
     } ); 
         }
         else if(check=="delivered"){
+             $scope.past = true;
              var filterdata1 = [];
              var m=0;
              $scope.noongo=false;
@@ -120,8 +139,14 @@ angular.module('halanxApp')
         for(var i=0;i<data.length;i++){
                      for(var j=0;j<data[i].order_items.length;j++){
                         //  data[i].Total = data[i].Total.toFixed(2);
-                          if(data[i].order_items[j].IsDelivered==true){
+                          if(data[i].order_items[j].IsDelivered==true || data[i].IsCancelled==true){
                             //   console.log(data[i]);
+                              if(data[i].IsDelivered == true){
+                                  data[i].order_status = "Delivered";
+                              }
+                              else if(data[i].IsCancelled == true){
+                                  data[i].order_status = "Cancelled";
+                              }
                               filterdata1[m] = data[i];
                               m++;
                               break;
@@ -163,4 +188,40 @@ angular.module('halanxApp')
     }
     }
      $scope.myorder();
+     $scope.openCancel = (id)=>{
+         if($scope.check1!=id){
+         $scope.check1 = id;
+        }
+        else{
+            $scope.check1 = "0000000";
+        }
+        //  console.log($scope.cancel_order);
+        //  $scope.cancel_order = !$scope.cancel_order;
+     }
+
+     $scope.cancelOrder = (id)=>{
+        $scope.order_id = id; 
+        $scope.cancelMsg = "Are you sure, you want to cancel the order?";
+        $scope.showCancel = false;   
+     }
+
+     $scope.confirmDeletion = ()=>{
+         $scope.showCancel = true;
+         let token = orders.gettoken();
+         console.log("token ",token);
+         orders.cancelOrder(token,$scope.order_id).then((data)=>{
+             $scope.showCancel = true;
+            
+           if(data.data.result == "Order cancelled successfully!"){
+                $scope.cancelMsg = "Order cancelled successfully!";
+                $scope.myorder("ongoing");
+            }
+            else{
+                $scope.cancelMsg = "Sorry, Our shopper is out on the delivery of your order, so can't be cancelled now.";
+            }
+         },(err)=>{
+             $scope.showCancel = true;
+             $scope.cancelMsg = "Sorry, Our shopper is out on the delivery of your order, so can't be cancelled now.";
+         });
+     }
   });
