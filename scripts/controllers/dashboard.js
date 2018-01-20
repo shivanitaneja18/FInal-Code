79f883130fc644fe1c30cd501aa5b400a3a013a4
Shivani Taneja <shivani.taneja104@gmail.com>
2018-01-20 13:02:42 +0530

    Update dashboard.js

--- scripts/controllers/dashboard.js
+++ scripts/controllers/dashboard.js
@@ -18,9 +18,13 @@ angular.module('halanxApp')
     var promise = dashboard.DashCall(token);
 
     $scope.dashdata = [];
-
+    $scope.payment = [];
     promise.then(function(data){
       console.log(data);
+      $scope.paid = data.data.paid;
+      $scope.pending = data.data.pending;
+      localStorage.setItem("paid_amount",data.data.paid);
+      localStorage.setItem("pending_amount",data.data.pending);
       $scope.dashdata = data.data.items;
       console.log("data is:",data.data.items);
     },function(err){
@@ -30,5 +34,16 @@ angular.module('halanxApp')
     $scope.visit = ()=>{
       $window.location.assign("#business");
       $window.location.reload();
+    }
+
+    $scope.openMe = ()=>{
+      // dashboard.PaymentCall(token).then((data)=>{
+      //   $scope.payment = data.data;
+      //   console.log(data.data);
+        
+      // })
+      for(var i=0;i<20;i++){
+      $scope.payment.push({id: i, amount: 1000*i, paid_on: "2018-01-11T12:30:00Z", store: 78});
+      }
     }
   });
